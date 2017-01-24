e3d72bb6c3023c7d5a7cdcd4de4983ac2d157fcb
nmd001 <nmd001@csclab-07.lvc.edu>
2017-01-24 15:03:29 -0500

    push

--- 6.1 & 6.3/src/QuickSort.java
+++ 6.1 & 6.3/src/QuickSort.java
@@ -1,6 +1,6 @@
 import java.util.Random;
 
-public class Main {
+public class QuickSort {
 
 	public static void main(String[] args) {
 		System.out.printf("%-20s%-20s%-20s%-20s%n", "Sort", "Size", "Comparison", "Time");
