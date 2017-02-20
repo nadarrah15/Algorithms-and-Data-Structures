253d958ac5a0da0b22ea06d3d06c90329326c72a
nmd001 <nmd001@csclab-05.lvc.edu>
2017-02-20 14:50:09 -0500

    k

--- BinaryTree/.svn/pristine/ef/ef0a23cc12f57f5a5368e7594a458c96aa3ab32f.svn-base
+++
@@ -1,9 +0,0 @@
-
-public class TestTrees {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
-}
--- BinaryTree/src/lvc/cds/trees/TreeIterator.java
+++
@@ -1,6 +0,0 @@
-package lvc.cds.trees;
-import java.util.Iterator;
-
-public abstract class TreeIterator<E> implements Iterator<E> {
-	
-}
