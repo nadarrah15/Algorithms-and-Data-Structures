4ba850a04595950c04b6dcaf5c33e8beb24afcc5
nmd001 <nmd001@lyn188-06>
2017-03-20 10:26:49 -0400

    01101011

--- ForQuiz4/src/bits/Test.java
+++ ForQuiz4/src/bits/Test.java
@@ -15,9 +15,9 @@ public class Test {
 		return ((i & (1 << 9)) == 0); //1 << 9 == 0001 0000 0000
 	}
 	
-	static void printBits(int i){ 	//works for negative numbers, but adds a 1 to the beginning of positive number
+	static void printBits(int i){
 		for(int j = 31; j >= 0; j--){
-			int bitmask = i & (1 << j);
+			int bitmask = i & (1 << j); //check for each index to see if it is 1 or 0
 			if(bitmask == 0)
 				System.out.print(0);
 			else
