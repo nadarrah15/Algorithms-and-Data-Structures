468f185e5e842971380a854e5d97f25f56316f60
Nathan <nadarrah15@gmail.com>
2017-03-30 21:48:55 -0400

    k

--- ForQuiz4/src/bits/Test.java
+++ ForQuiz4/src/bits/Test.java
@@ -7,17 +7,17 @@ public class Test {
 	}
 	
 	static int setTenBitsToTheRight(int i){
-		int j = i | (1 << 10); //1 << 10 == 0010 0000 0000 
-		return j;
+		i |= (1 << 10); //1 << 10 == 0010 0000 0000 
+		return i;
 	}
 	
 	static boolean isNinthBitZero(int i){
 		return ((i & (1 << 9)) == 0); //1 << 9 == 0001 0000 0000
 	}
 	
-	static void printBits(int i){ 	//works for negative numbers, but adds a 1 to the beginning of positive number
+	static void printBits(int i){
 		for(int j = 31; j >= 0; j--){
-			int bitmask = i & (1 << j);
+			int bitmask = i & (1 << j); //check for each index to see if it is 1 or 0
 			if(bitmask == 0)
 				System.out.print(0);
 			else
